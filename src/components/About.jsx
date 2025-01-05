b3a2460f3bb0f15f1308cc010530bdda0ddbf35b
abderrahmane140 <absarabdo4@gmail.com>
2025-01-05 23:30:52 +0100

    create about component

---
+++ src/components/About.jsx
@@ -0,0 +1,44 @@
+export default function About() {
+  return (
+    <section id="about">
+      <div
+        className="flex flex-col justify-center gap-6 text-white px-8 h-screen "
+      >
+        <p>
+          I’m a full-stack developer passionate about crafting seamless digital
+          experiences that blend innovation with usability. I specialize in
+          building dynamic, scalable web applications with a focus on
+          accessibility, performance, and pixel-perfect design. My expertise spans
+          the entire development stack—from creating intuitive front-end
+          interfaces to designing robust back-end architectures that deliver
+          reliability and efficiency.
+        </p>
+        <p>
+          Beyond my development work, I have a strong inclination to share the
+          knowledge I've accumulated throughout my journey in Web Development.
+          You're invited to connect with me on{" "}
+          <a
+            className="text-lightGreen underline"
+            href="https://www.linkedin.com/in/abderrahman-bsar"
+          >
+            LinkedIn
+          </a>
+          , where I regularly share insightful content related to Web Development
+          and Programming. I believe in fostering a thriving Dev Community by
+          imparting my expertise and experiences.
+        </p>
+        <p>
+          I'm open to Job opportunities where I can contribute, learn, and grow.
+          If you have a good opportunity that matches my skills and experience,
+          then don't hesitate to{" "}
+          <a
+            className="text-lightGreen underline"
+            href="mailto:bsarabderrahmane01@gmail.com"
+          >
+            contact me.
+          </a>
+        </p>
+      </div>
+    </section>
+  );
+}
